68de7ddb6fe92bcdc3270be1410c4699978138fb
Abdusamatov-Samandar <abdusamatovsamandardeveloper@gmail.com>
2024-07-05 15:59:51 +0500

    edit

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 const Category = ({image, title,}) => {
   return (
     
-        <li className="flex justify-between gap-3 max-w-[230px] min-w-[230px]
+        <li className="flex justify-start gap-3 max-w-[230px] min-w-[230px]
          py-5 px-12 mb-3 shadow-md bg-white rounded">
             <img className="" src={image} width={26} height={24} alt="icon" />
             <h3>{title}</h3>
